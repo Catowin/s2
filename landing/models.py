3c8876f506551c7b51fab273871572e3139c991a
Your Name <catowin.alia@gmail.com>
2017-12-28 14:26:16 +0200

    'css'

--- landing/models.py
+++ landing/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
-class Subscribers (models.Model):
+class Subscriber (models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=256)
+
+
+    def __str__(self):
+        return ' Пользователь {} Имейл {}'.format(self.name, self.email)
+
+    class Meta:
+        verbose_name = 'MySubscriber'
+        verbose_name_plural = 'A lot of Subscribers'
+
